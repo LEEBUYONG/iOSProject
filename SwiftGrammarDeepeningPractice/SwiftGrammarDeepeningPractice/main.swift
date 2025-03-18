c161b627dd6631fc13dbdf7979a225f680bde9e0
LEEBUYONG <dqd001@naver.com>
2025-03-18 16:00:26 +0900

    feat: 3주차 과제 LV1. 클로저 구현

---
+++ SwiftGrammarDeepeningPractice/SwiftGrammarDeepeningPractice/main.swift
@@ -0,0 +1,23 @@
+// ===== 3주차 문법 심화 과제 =====
+
+import Foundation
+
+// ===== LV 1. 클로저 심화 실습하기 =====
+// --- 요구사항 1, 2. 클로저 정의 및 클로저 상수 sum에 저장 ---
+let sum: (Int, Int) -> String = {
+    let result: Int = $0 + $1
+    return "두 수의 합은 \(result) 입니다"
+}
+
+// --- 요구사항 3. sum 호출 ---
+print(sum (10,20))
+
+// --- 요구사항 4. 클로저를 파라미터로 받고 반환 값이 없는 함수 작성 및 호출 ---
+func calculate(closure: (Int, Int) -> String, _ a: Int, _ b: Int) {
+    let result = closure(a,b)
+    print(result)
+    
+}
+
+calculate(closure: sum, 10, 30)
+
