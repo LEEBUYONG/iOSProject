ad242ea631239b806052ada651efbc480c3a92ed
LEEBUYONG <dqd001@naver.com>
2025-05-08 12:17:16 +0900

    [Setting] - 넷플리스 클론 앱 세팅

---
+++ NetflixCloneApp_BY/NetflixCloneApp_BY/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixCloneApp_BY
+//
+//  Created by iOS study on 5/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
