f8618f9cb57b0b1ec2feb90352d6517222630a9e
LEEBUYONG <dqd001@naver.com>
2025-05-08 13:51:16 +0900

    [Feat] - Response 구조체 선언

--- NetflixCloneApp_BY/NetflixCloneApp_BY/Model/Movie.swift
+++ NetflixCloneApp_BY/NetflixCloneApp_BY/Model/Movie.swift
@@ -5,3 +5,19 @@
 //  Created by iOS study on 5/8/25.
 //
 
+import Foundation
+
+struct MovieResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int?
+    let title: String?
+    let posterPath: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title
+        case posterPath = "poster_path"
+    }
+}
--- NetflixCloneApp_BY/NetflixCloneApp_BY/Model/Viedeo.swift
+++ NetflixCloneApp_BY/NetflixCloneApp_BY/Model/Viedeo.swift
@@ -5,3 +5,14 @@
 //  Created by iOS study on 5/8/25.
 //
 
+import Foundation
+
+struct VideoResponse: Codable {
+    let results: [Video]
+}
+
+struct Video: Codable {
+    let key: String?
+    let site: String?
+    let type: String?
+}
