48c07a0ad77729a600f9f795f3d56e631abcaa90
LEEBUYONG <dqd001@naver.com>
2025-05-08 15:37:58 +0900

    [Feat] - NetworkManager 작성
    데이터를 가져올 때 중복되는 코드를 줄이기 위해 싱글톤으로 NetworkManager를 생성 및 구현

---
+++ NetflixCloneApp_BY/NetflixCloneApp_BY/ViewModel/MainViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewModel.swift
+//  NetflixCloneApp_BY
+//
+//  Created by iOS study on 5/8/25.
+//  서버로부터 데이터를 불러오는 비즈니스 로직 작성
+
+import UIKit
+
+class MainViewModel {
+    private let apiKey = "de77c9b9cdd46c2cc85f038a2b0d2003"
+    
+    init() {
+        
+    }
+    
+    // Populatr Move 데이터를 불러옴
+    func fetchPoppularMovie() {
+        
+    }
+    
+    // TopRated Move 데이터를 불러옴
+    func fetchTopRatedMovie() {
+        
+    }
+    
+    // Upcoming Move 데이터를 불러옴
+    func fetchUpcomingMovie() {
+        
+    }
+    
+    
+}
+
