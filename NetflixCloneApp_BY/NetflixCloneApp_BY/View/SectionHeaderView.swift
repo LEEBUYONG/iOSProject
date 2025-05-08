f62f4d211c4b883893eecfca48c5e611452d4cd9
LEEBUYONG <dqd001@naver.com>
2025-05-08 14:14:27 +0900

    [Feat] - SectionHeaderView 작성 (titleLabel)

--- NetflixCloneApp_BY/NetflixCloneApp_BY/View/SectionHeaderView.swift
+++ NetflixCloneApp_BY/NetflixCloneApp_BY/View/SectionHeaderView.swift
@@ -5,4 +5,40 @@
 //  Created by iOS study on 5/8/25.
 //
 
-import Foundation
+import UIKit
+
+import Then
+
+class SectionHeaderView: UICollectionReusableView {
+    static let id = "SectionHeaderView"
+    
+    let titleLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        $0.textColor = .white
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
+        ])
+    }
+    
+    func configure(title: String) {
+        titleLabel.text = title
+    }
+}
+
